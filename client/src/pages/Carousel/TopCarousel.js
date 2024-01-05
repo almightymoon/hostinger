470ed51309a06ce069ac8ec0aa278eca9d98d749
almightymoon <atharqulimoon@gmail.com>
2024-01-05 13:51:03 +0500

    first commit

---
+++ client/src/pages/Carousel/TopCarousel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const TopCarousel = () => {
+  return (
+    <div
+      className='hero-sec flex items-center justify-center h-96 md:h-550 lg:w-full '
+      style={{ marginTop: "-3%" }}>
+    </div>
+  );
+};
+
+export default TopCarousel;
