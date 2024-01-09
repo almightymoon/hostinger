abf251ff9555cee964c918cfceb1e205c4423517
AM <am@MacBook-Pro-15-inch-2018.local>
2024-01-09 07:04:01 -0800

    code updated

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -52,19 +52,19 @@ export default function Header() {
     <Disclosure as="nav" className="bg-white-800 mt-4">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-4">
+          <div className="mx-auto px-2 sm:px-6 lg:px-8 mt-4">
             <div className="relative flex h-6 items-center justify-between">
-            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-    <span className="absolute -inset-0.5" />
-    <span className="sr-only">Open main menu</span>
-    {open ? (
-      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-    ) : (
-      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-    )}
-  </Disclosure.Button>
-</div>
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
 
               <div className="flex sm:px-6 items-center cursor-pointer">
                 <h1>
@@ -76,34 +76,34 @@ export default function Header() {
                   <div className="flex space-x-4 ">
                     {navigation.map((item) => (
                       <div
-                        className="relative group"
                         key={item.name}
-                        // Add "hidden" class for small screens
-                        ClassName={`${
-                          item.name !== "CATEGORY" ? "hidden sm:block" : ""
-                        }`}>
-                        <Link
-                          key={item.name}
-                          to={item.to}
+                        className={`relative group ${item.name !== "CATEGORY" ? "hidden sm:block" : ""}`}
+                      >
+                        <Disclosure.Button
+                          as="a"
+                          href={item.to}
                           className={classNames(
                             item.current
                               ? "bg-gray-400 text-white mr-6"
                               : "text-gray-900 hover:bg-gray-400 hover:text-white",
                             "rounded-md px-3 py-2 text-sm font-medium"
                           )}
-                          aria-current={item.current ? "page" : undefined}>
+                          aria-current={item.current ? "page" : undefined}
+                        >
                           {item.name}
-                        </Link>
+                        </Disclosure.Button>
                         {item.name === "CATEGORY" && (
-                          <div className="hidden group-hover:block absolute  right-0 left-5 z-10 mt-2 w-40  text-center rounded-md bg-white  border-2 border-blue-400 focus:outline">
+                          <div className="hidden group-hover:block absolute right-0 left-5 z-10 mt-2 w-40 text-center rounded-md bg-white border-2 border-blue-400 focus:outline">
                             <Link
                               to="#"
-                              className="px-0 py-0 text-md text-right text-gray-700">
+                              className="px-0 py-0 text-md text-right text-gray-700"
+                            >
                               Floor Tiles
                             </Link>
                             <Link
                               to="#"
-                              className="block px-0 py-0 text-sm text-gray-700">
+                              className="block px-0 py-0 text-sm text-gray-700"
+                            >
                               Submenu Item 2
                             </Link>
                           </div>
@@ -113,12 +113,13 @@ export default function Header() {
                   </div>
                 </div>
               </div>
-              <div className="hidden sm:flex  flex items-end  space-x-4">
+              <div className="hidden sm:flex flex items-end space-x-4">
                 <div className="flex items-center ">
                   <Link
                     to="#"
                     className="ml-4 text-gray-800 hover:text-gray-300"
-                    onClick={handleSearchIconClick}>
+                    onClick={handleSearchIconClick}
+                  >
                     <MagnifyingGlassPlusIcon
                       className="h-6 w-6"
                       aria-hidden="true"
@@ -129,7 +130,8 @@ export default function Header() {
                 <Badge count={cart?.length} showZero offset={[10, -5]}>
                   <Link
                     to="/cart"
-                    className="ml-4 text-gray-800 hover:text-gray-300">
+                    className="ml-4 text-gray-800 hover:text-gray-300"
+                  >
                     <ShoppingCartIcon
                       className="h-6 w-6"
                       aria-hidden="true"
@@ -138,24 +140,29 @@ export default function Header() {
                 </Badge>
                 <div>
                   {auth.user ? (
-                    <Link
-                      to="/logout"
+                    <a
+                      onClick={handleLogout}
                       className="ml-4 text-gray-800 hover:text-gray-300"
-                      onClick={handleLogout}>
+                      data-headlessui-state="open"
+                    >
                       Logout
-                    </Link>
+                    </a>
                   ) : (
                     <>
-                      <Link
-                        to="/login"
-                        className="ml-4 text-gray-800 hover:text-gray-300">
+                      <a
+                        href="/login"
+                        className="ml-4 text-gray-800 hover:text-gray-300"
+                        data-headlessui-state="open"
+                      >
                         Login
-                      </Link>
-                      <Link
-                        to="/register"
-                        className="ml-4 text-gray-800 hover:text-gray-300">
+                      </a>
+                      <a
+                        href="/register"
+                        className="ml-4 text-gray-800 hover:text-gray-300"
+                        data-headlessui-state="open"
+                      >
                         Register
-                      </Link>
+                      </a>
                     </>
                   )}
                 </div>
@@ -163,39 +170,57 @@ export default function Header() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-  <div className="space-y-1 px-2 pb-3 pt-2">
-    {navigation.map((item) => (
-      <Disclosure.Button
-        key={item.name}
-        as="a"
-        href={item.to}
-        className={classNames(
-          item.current
-            ? "bg-gray-900 text-white"
-            : "text-black hover:bg-gray-700 hover:text-white", // Change color to black
-          "block rounded-md px-3 py-2 text-base font-medium"
-        )}
-        aria-current={item.current ? "page" : undefined}
-      >
-        {item.name}
-      </Disclosure.Button>
-    ))}
-    {/* Add conditional rendering for login/logout options */}
-    {auth.user ? (
-      <div className="flex flex-col space-y-2">
-        <a onClick={handleLogout} class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" data-headlessui-state="open">logout</a>
-      </div>
-    ) : (
-      <div className="flex flex-col space-y-2">
-       <a href="/login" class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" data-headlessui-state="open">Login</a>
-        <a href="/register" class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" data-headlessui-state="open">Register</a>
-      </div>
-    )}
-    {/* End of conditional rendering */}
-  </div>
-</Disclosure.Panel>
 
+
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.to}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-black hover:bg-gray-700 hover:text-white", // Change color to black
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+              {auth.user ? (
+                <div className="flex flex-col space-y-2">
+                  <a
+                    onClick={handleLogout}
+                    className="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                    data-headlessui-state="open"
+                  >
+                    Logout
+                  </a>
+                </div>
+              ) : (
+                <div className="flex flex-col space-y-2">
+                  <a
+                    href="/login"
+                    className="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                    data-headlessui-state="open"
+                  >
+                    Login
+                  </a>
+                  <a
+                    href="/register"
+                    className="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                    data-headlessui-state="open"
+                  >
+                    Register
+                  </a>
+                </div>
+              )}
+            </div>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
